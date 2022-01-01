01af6ed59f245813bca4bfe2f822f998c8726c39
Daniel Ryu <rujjiryu99@gmail.com>
2022-01-01 04:01:19 -0800

    add trait stubs for Chunk

--- src/chunk.rs
+++ src/chunk.rs
@@ -1,3 +1,5 @@
+use std::fmt;
+
 use crate::chunk_type::ChunkType;
 use crate::{Error, Result};
 
@@ -6,4 +8,18 @@ pub struct Chunk<'a> {
     r#type: ChunkType,
     data: &'a [u8],
     crc: u32, // TODO maybe [u8; 4]
+}
+
+impl TryFrom<&[u8]> for Chunk<'_> {
+    type Error = Error;
+
+    fn try_from(bytes: &[u8]) -> Result<Self> {
+        todo!()
+    }
+}
+
+impl fmt::Display for Chunk<'_> {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        todo!()
+    }
 }
